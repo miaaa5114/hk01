1cb6b691d4c40ba74b913ad00fc869b474193e41
miaaa5114 <160557637+miaaa5114@users.noreply.github.com>
2024-02-27 12:14:34 +0800

    Add files via upload

---
+++ hk1/src/hw1/hk01.java
@@ -0,0 +1,27 @@
+package hw1;
+
+import java.util.Scanner;
+
+public class hk01 {
+    
+	static int pv;
+	static int fv;
+	static final double interest=0.05;
+	static int interval;
+
+	public static void main(String[] args) { 
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.print("pv");
+	pv=sc.nextInt();
+	System.out.print("interval");
+	interval=sc.nextInt();
+	
+	
+
+	fv=(int) (pv* Math.pow((1+interest), interval));
+
+	System.out.println("My pv is"+pv+" and after "+ interval +"year it would be " +fv);
+
+	}
+}
